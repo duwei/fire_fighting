662fff5e97b33cb2903a76acb54ba8f8c1f27360
whhgn <liuy@whhgn.cn>
2012-08-29 17:47:37 +0800

    汇报版本(1)

---
+++ root/main/app/store/JingGWZhs.js
@@ -0,0 +1,10 @@
+Ext.define('FV.store.JingGWZhs', {// 警官文职 是否文职干部
+    extend: 'Ext.data.Store',
+
+    model: 'FV.model.LabelValue',
+
+    data: [
+        {value: 0,   label: '警官'},
+        {value: 1,   label: '文职'}
+	]
+});
