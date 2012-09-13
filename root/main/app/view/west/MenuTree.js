e141e729eb2a1e0409a9ba666af8c314b753a521
whhgn <liuy@whhgn.cn>
2012-09-13 11:56:36 +0800

    a

--- root/main/app/view/west/MenuTree.js
+++ root/main/app/view/west/MenuTree.js
@@ -15,8 +15,8 @@ Ext.define('FV.view.west.MenuTree', {
 				children: [
 					{ text: "综合查询", leaf: true, url:'/main/sch.app',iframe:true },
 					{ text: "用户管理", leaf: true },
-					{ text: "系统安全", leaf: true },
-					{ text: "操作日志", leaf: true }
+					{ text: "系统安全", leaf: true },// 数据库手工备份／恢复
+					{ text: "操作日志", leaf: true } // 查询修改日志，详细修改信息，只针对人员操作，包括报审内容
 				]
 			}
         });
