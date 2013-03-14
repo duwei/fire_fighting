4c995c538cb47dda34c40cf714ff066c7e8c8091
whhgn <whhgn@YAPC-1007.(none)>
2013-03-14 20:43:51 +0800

    导出《警官任免晋衔报告表》只把表头填上（部分代码）

--- set_evn.bat
+++ set_evn.bat
@@ -1,7 +1,7 @@
 @echo off
 set TCC=%~dp0tcc
 set SERV=%~dp0serv
-set INC_OPT=-I%SERV%\include -I%SERV%\base -I%SERV%\include\apr2 -I%tcc%\include\winapi
+set INC_OPT=-I%SERV%\include -I%SERV%\base -I%SERV%\include\apr2 -I%SERV%\include\opc -I%tcc%\include\winapi
 set LIB_OPT=-L%SERV%\lib -lapr-2 -lmongoose -lfun_subr
 set TCC_OPT=%INC_OPT% %LIB_OPT%
 
