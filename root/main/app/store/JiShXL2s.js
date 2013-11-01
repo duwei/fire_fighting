d63b68fe24e7c16f5140fcabe20296e9ae15b3c9
liuying <liuying@whhgn.cn>
2013-11-01 16:47:25 +0800

    update2013102600完善2

--- root/main/app/store/JiShXL2s.js
+++ root/main/app/store/JiShXL2s.js
@@ -5,20 +5,12 @@ Ext.define('FV.store.JiShXL2s', {//技术系列 专业系统，独立输入用�
 
     data: [
         {value: null,   label: '请选择...'},
-        {value: 1,   label: '工程系列'},
-        {value: 11,  label: '工程系列-防火监督专业'},
-        {value: 21,  label: '工程系列-通信、计算机专业'},
-        {value: 31,  label: '工程系列-灭火救援专业'},
-        {value: 41,  label: '工程系列-消防装备专业'},
-        {value: 2,   label: '医学系列'},
-        {value: 12,  label: '医学系列-技师专业'},
-        {value: 3,   label: '护理学系列'},
-        {value: 4,   label: '会计系列'},
-        {value: 5,   label: '审计系列'},
-        {value: 6,   label: '记者系列'},
-        {value: 7,   label: '编辑系列'},
-        {value: 8,   label: '出版系列'},
-        {value: 9,   label: '教育系列'},
-        {value: 0,   label: '_'}
+        {value: 1,  label: '工程系列-防火监督专业'},
+        {value: 2,  label: '工程系列-通信、计算机专业'},
+        {value: 3,  label: '工程系列-灭火救援专业'},
+        {value: 4,  label: '工程系列-消防装备专业'},
+        {value: 5,  label: '会（审）计系列'},
+        {value: 6,  label: '卫生系列'},
+        {value: 7,  label: '新闻系列'},
 	]
 });
